950686b6945ab642868b0ac89ac8841b4d77ebf8
Tiago Neves <tiagoneves.tia@gmail.com>
2024-03-07 20:51:55 -0300

    feat: add tipos de dados para nome, tags, ingredientes e instruções da receita

--- src/domain/recipe.rs
+++ src/domain/recipe.rs
@@ -2,14 +2,74 @@ use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct RecipeName(String);
+
+impl TryFrom<String> for RecipeName {
+    type Error = &'static str;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            Err("A receita precisa ter um nome")
+        } else {
+            Ok(RecipeName(value))
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct RecipeTags(Vec<String>);
+
+impl TryFrom<Vec<String>> for RecipeTags {
+    type Error = &'static str;
+
+    fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            Err("A receita precisa pelo menos de uma tag")
+        } else {
+            Ok(RecipeTags(value))
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct RecipeIngredients(Vec<String>);
+
+impl TryFrom<Vec<String>> for RecipeIngredients {
+    type Error = &'static str;
+
+    fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            Err("A receita precisa pelo menos de um ingrediente")
+        } else {
+            Ok(RecipeIngredients(value))
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct RecipeInstructions(Vec<String>);
+
+impl TryFrom<Vec<String>> for RecipeInstructions {
+    type Error = &'static str;
+
+    fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            Err("A receita precisa pelo menos de uma instrução")
+        } else {
+            Ok(RecipeInstructions(value))
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Recipe {
-    id: Option<Uuid>,
-    name: String,
-    tags: Vec<String>,
-    ingredients: Vec<String>,
-    instructions: Vec<String>,
-    published_at: Option<DateTime<Local>>,
+    pub id: Option<Uuid>,
+    pub name: RecipeName,
+    pub tags: RecipeTags,
+    pub ingredients: RecipeIngredients,
+    pub instructions: RecipeInstructions,
+    pub published_at: Option<DateTime<Local>>,
 }
 
 impl Recipe {
@@ -19,30 +79,27 @@ impl Recipe {
         ingredients: Vec<String>,
         instructions: Vec<String>,
     ) -> Result<Self, String> {
-        if name.is_empty() {
-            return Err("A receita precisa ter um nome".to_string());
-        }
-
-        if tags.is_empty() {
-            return Err("A receita precisa pelo menos de uma tag".to_string());
-        }
-
-        if ingredients.is_empty() {
-            return Err("A receita precisa pelo menos de um ingrediente".to_string());
-        }
-
-        if instructions.is_empty() {
-            return Err("A receita precisa pelo menos de uma instrução".to_string());
-        }
+        let recipe_name = RecipeName::try_from(name)?;
+        let recipe_tags = RecipeTags::try_from(tags)?;
+        let recipe_ingredients = RecipeIngredients::try_from(ingredients)?;
+        let recipe_instructions = RecipeInstructions::try_from(instructions)?;
 
         Ok(Recipe {
             id: Some(Uuid::now_v7()),
-            name,
-            ingredients,
-            instructions,
-            tags,
+            name: recipe_name,
+            ingredients: recipe_ingredients,
+            instructions: recipe_instructions,
+            tags: recipe_tags,
             published_at: Some(Local::now()),
         })
+    }
+
+    pub fn name(&self) -> &RecipeName {
+        &self.name
+    }
+
+    pub fn tags(&self) -> &RecipeTags {
+        &self.tags
     }
 }
 
@@ -55,7 +112,7 @@ mod tests {
         let tags = vec!["main".to_string(), "chicken".to_string()];
         let ingredients = vec!["4 (6 to 7-ounce) boneless skinless chicken breasts\r".to_string()];
         let instructions = vec!["To marinate the chicken: In a non-reactive dish, combine the lemon juice, olive oil, oregano, salt, and pepper and mix together".to_string()];
-        let name = "Oregano Marinated Chicken";
+        let name: &str = "Oregano Marinated Chicken";
 
         let new_recipe = Recipe::new(
             name.to_string(),
@@ -65,10 +122,11 @@ mod tests {
         )
         .unwrap();
 
-        assert_eq!(new_recipe.name, name);
-        assert_eq!(ingredients.len(), new_recipe.ingredients.len());
+        assert_eq!(new_recipe.name.0, name);
+        assert_eq!(tags.len(), new_recipe.tags.0.len());
+        assert_eq!(ingredients.len(), new_recipe.ingredients.0.len());
         for (i, exp_ins) in instructions.into_iter().enumerate() {
-            assert_eq!(exp_ins, new_recipe.instructions[i])
+            assert_eq!(exp_ins, new_recipe.instructions.0[i])
         }
     }
 
